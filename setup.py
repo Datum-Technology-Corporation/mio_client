be4f662446bdfb8d4eac412d9ceaf8b76d2bdb7f
datum-dpoulin <dpoulin@datumtc.ca>
2022-03-18 16:47:53 -0400

    Cleaning up project metadata

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 # Copyright Datum Technology Corporation
-# Copyright Moore.io contributors
 ########################################################################################################################
 # SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 ########################################################################################################################
@@ -36,20 +35,16 @@ setup(
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
         "Topic :: Utilities",
-        "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development :: Build Tools"
     ],
-    entry_points={"console_scripts": ["fusesoc = fusesoc.main:main"]},
+    entry_points={"console_scripts": ["mio = mio.__main__:main"]},
     setup_requires=[
         "setuptools_scm",
     ],
     install_requires=[
-        "edalize>=0.2.3",
-        "pyparsing",
-        "pyyaml",
-        "simplesat>=0.8.0",
+        "docopt",
+        "fusesoc"
     ],
     # Supported Python versions: 3.6+
     python_requires=">=3.6, <4",
